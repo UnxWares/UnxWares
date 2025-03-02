2b9316e9b869ebcd2353eaa2e3b97edcdefc7582
Baptiste Gosselin <contact@baptiste-gosselin.fr>
2025-03-02 13:31:16 +0100

    Add SEO

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>UnxWares - Welcome</title>
+
+	<meta property="og:title" content="UnxWares - Welcome">
+	<meta name="twitter:title" content="UnxWares - Welcome">
+
+	<meta property="og:url" content="https://www.unxwares.com">
+	<meta name="twitter:site" content="https://www.unxwares.com">
+</svelte:head>
